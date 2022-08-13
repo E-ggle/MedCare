4d6cbe21a2b84109b772fea5495d90179c98be4a
Vairamuthu <mvairamuthu2003@gmail.com>
2022-08-13 20:12:09 +0530

    added header

--- medcare/src/Components/Header.js
+++ medcare/src/Components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Header.css";
+import services from "../images/HEART.png";
 
 function Header() {
   return (
     <div className="header">
-      <span>
-        <text>MedCare</text>
+      <span className="leftcell">
+        <img className="services-img" src={services} alt="" />
+        <text className="lefttitle">MedCare</text>
       </span>
-      <span>
-        <button>Get Services</button>
+      <span className="rightcell">
+        <button className="service-button">Get Services</button>
       </span>
     </div>
   );
