7f92f36009bda4eb3e1ca68ddb0c03787e831ccf
Vairamuthu <mvairamuthu2003@gmail.com>
2022-08-13 15:03:27 +0530

    added initial header,footer, home page

---
+++ medcare/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <span>
+        <text>MedCare</text>
+      </span>
+      <span>
+        <button>Get Services</button>
+      </span>
+    </div>
+  );
+}
+
+export default Header;
